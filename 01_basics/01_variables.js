b1d6673e41ae704c80460aa8b8de846d3ddd1496
Sourojit Banerjee <181811476+Sourojitbanerjee@users.noreply.github.com>
2025-07-09 01:35:30 +0530

    js basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "contact.glsouronline@gmail.com"
+var accountPassouro = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2
+
+
+accountEmail = "vmvmbav.com"
+accountPassword = "123456789"
+aacountCity = "kolkata"
+console.log(accountId);
+
+//prefer not to use var 
+// because os issue in block scope and functional scope 
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";//trreat all js code as newer version
+
+// alert("hello") we are using node js not browser
+
+console.log(3 + 3);
+
+console.log("souro")
+// code readability should be high
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
