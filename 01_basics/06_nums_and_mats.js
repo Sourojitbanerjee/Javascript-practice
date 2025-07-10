0e0471e9a0ac44060420e3f817ea09f28637bfac
Sourojit Banerjee <181811476+Sourojitbanerjee@users.noreply.github.com>
2025-07-10 16:17:16 +0530

    js prac

---
+++ 01_basics/06_nums_and_mats.js
@@ -0,0 +1,19 @@
+const score = 400
+console.log(score);
+
+
+const balance = new Number(100)
+
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
+const number2 = 2390.79
+console.log(number2.toPrecision(4));
+
+
+const hundreds = 10000
+console.log(hundreds.toLocaleString('en-in'));
+console.log('souro')
