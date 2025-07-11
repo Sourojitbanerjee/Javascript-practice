6ee628f82482718de1a86fa7339512c4d3940728
Sourojit Banerjee <181811476+Sourojitbanerjee@users.noreply.github.com>
2025-07-11 16:17:12 +0530

    js prac

---
+++ 02_basics/03_objests.js
@@ -0,0 +1,19 @@
+// singleton
+
+// object literals
+
+const JsUser = {
+    name: "souro",
+    "fulname":"sourojit Banerjee",
+    age: 19,
+    locattion: "kolkata",
+    email: "souro@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays:["Thursday","Monday"]
+
+}
+
+console.log(JsUser.email)
+console.log(JsUser["email"])
+console.log(JsUser.fullname)
+
