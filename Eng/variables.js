37582262e30f2e761fb116034800c40048115b84
Sourojit Banerjee <181811476+Sourojitbanerjee@users.noreply.github.com>
2025-08-05 11:51:45 +0530

    js prac

---
+++ Eng/variables.js
@@ -0,0 +1,40 @@
+// let age = 25
+// console.log(`your age is ${age}`);
+
+// let price = 10.99
+// console.log(`the price is ${price}`)
+
+
+// let gpa = 6.41
+// console.log(`your gpa is ${gpa}`)
+
+// console.log(typeof age);
+
+// let firstname = "sourojit"
+// let favouritefood = "chicken"
+// let email = "contact.glsouronline@gmail.com"
+
+// console.log(typeof firstname)
+// console.log(typeof favouritefood)
+// console.log(`your first name is ${firstname}`)
+// console.log(`you like is ${favouritefood}`)
+// console.log(`your email is ${email}`)
+
+// let online = true;
+// let forsale = false;
+// let isStudent = true;
+
+// console.log(typeof(online))
+// console.log(`bro is online: ${online}`)
+// console.log(`is this car forsale: ${forsale}`)
+// console.log (`enrolled: ${isStudent}`)
+
+let fullname = "Sourojit"
+let age = 19
+let student = true
+
+document.getElementById("p1").textContent = `Your name is ${fullname}`;
+document.getElementById("p2").textContent = `you are ${age} years old`;
+document.getElementById("p3").textContent = `enroled: ${student}`;
+
+
