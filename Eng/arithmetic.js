f7e8ea58ffee078f1f300fc40aaee0c0aad20644
Sourojit Banerjee <181811476+Sourojitbanerjee@users.noreply.github.com>
2025-08-06 03:49:55 +0530

    arithmeticoperators

---
+++ Eng/arithmetic.js
@@ -0,0 +1,23 @@
+// let students = 60;
+// students = students + 1
+// students = students - 2
+// students = students*3
+// students = students /2
+// students = students ** 2
+// students = students % 23
+
+// studnets += 4
+// studnets -= 4
+//  studnets *= 4
+//  studnets /= 4
+// students %= 4
+// students **= 4
+
+// studnets++;
+// students--;
+// console.log(students);
+
+
+let result = 1 + 2 * 3 + 4 ** 2;
+console.log(result)
+
---
+++ Eng/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+studnets++;
+// students--;
